c23f463014fee8b0c72ea092bfe8cd0a3032fbc7
Nivetha <nivethsukumar21@gmail.com>
2020-07-29 21:51:58 +0200

    Stacks are fun

---
+++ stack/reverse-linkedlist-using-stack.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+Node* head;
+void Reverse(){
+    if (head == NULL) return;
+    stack <struct Node*> S;
+    Node *temp = head;
+
+    while(temp != NULL)
+    {
+        S.push(temp);
+        temp = temp->next;
+    }
+
+    temp = S.top(); head = temp;
+    S.pop();
+    while(!S.empty()){
+        temp->next = S.top();
+        S.pop();
+        temp = temp->next;
+    }
+    temp->next = NULL;
+
+}
+int main(){
+    head = NULL;
+   
+    Reverse();
+}
